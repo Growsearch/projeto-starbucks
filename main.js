63162ada294004835e7998c7b1b3f35f2ed3fb72
Grow Search <marcelocx1904@gmail.com>
2022-12-15 16:33:35 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+
+function imgSlider(anything) {
+    document.querySelector('.starbucks').src = anything;
+}
+
+function changeCircle(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
